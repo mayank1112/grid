bc854c39a5b5ffa4c69dee7f75ba68583622e71c
mayank1112 <mayank1112@gmail.com>
2021-04-24 04:26:17 +0100

    performance optimization

--- src/App.js
+++ src/App.js
@@ -1,91 +1,112 @@
 import logo from './logo.svg';
 import './App.css';
 import Slider from '@material-ui/core/Slider';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './style.css';
 import ColorPicker from 'rc-color-picker'
 import 'rc-color-picker/assets/index.css';
 
+const DEFAULT_SIZE = 3;
+const MIN_SIZE = 1;
+const MAX_SIZE = 4;
+let found = [];
+
 function App() {
-  const [value, setValue] = useState(5);
-  const [currI, setCurrI] = useState(-1);
-  const [currJ, setCurrJ] = useState(-1);
-  const [count, setCount] = useState('');
+  const [value, setValue] = useState(DEFAULT_SIZE);
+  const [currX, setCurrX] = useState(-1);
+  const [currY, setCurrY] = useState(-1);
   const [color, setColor] = useState('red');
   const [highlight, setHighlight] = useState('yellow');
   const [display, setDisplay] = useState(false);
   const [hover, setHover] = useState(false);
-  const [grid, setGrid] = useState([
-    [0, 0, 0, 0, 1],
-    [1, 1, 0, 0, 0],
-    [1, 1, 0, 1, 1],
-    [0, 0, 0, 0, 0],
-    [1, 1, 1, 0, 0],
-  ]);
+  const [grid, setGrid] = useState([]);
+
+  useEffect(() => {
+    generateGrid(value);
+  }, []);
+
+  const generateGrid = passedValue => {
+    let newGrid = [];
+    for (let i = 0; i < passedValue; i++) {
+      let row = [];
+      for (let j = 0; j < passedValue; j++) {
+        row.push(Math.round(Math.random(1)));
+      }
+      newGrid.push(row);
+    }
+    setGrid(newGrid);
+  };
+
   const handleValue = (event, passedValue) => {
     if (passedValue !== value) {
       setValue(passedValue);
-      let newGrid = [];
-
-      for (let i = 0; i < passedValue; i++) {
-        let row = [];
-        for (let j = 0; j < passedValue; j++) {
-          row.push(Math.round(Math.random(1)));
-        }
-        newGrid.push(row);
-      }
-      setGrid(newGrid);
-      setCount('');
+      generateGrid(passedValue)
     }
   };
 
+  const validSquare = (x, y) => x > -1 && x < value && y > -1 && y < value;
 
-  const check = (x, y) => {
-    if (grid[y][x] !== 1) {
-      setCount('');
-    } else {
+  const filledSquare = (x, y) => {
+    if (validSquare(x, y) && grid[y][x] === 1) {
+      if (!found.includes(`${x}${y}`)) {
+        found = [...found, `${x}${y}`];
+      }
+      return true;
+    }
+  }
+
+  const onMouseOver = (x, y) => {
+    found = [];
+    if (grid[y][x] === 1) {
       setHover(true);
-      setCurrI(x);
-      setCurrJ(y);
-      let newCount = 0;
-      for (let i = 0; i < value; i++) {
-        for (let j = 0; j < value; j++) {
-          if (i + 1 === x && j === y && grid[j][i] === 1 ||
-            i - 1 === x && j === y && grid[j][i] === 1 ||
-            i === x && j + 1 === y && grid[j][i] === 1 ||
-            i === x && j - 1 === y && grid[j][i] === 1) {
-            newCount++;
-          }
-        }
-      }
-      setCount(newCount);
+      setCurrX(x);
+      setCurrY(y);
+      filledSquare(x, y);
+      searchMore(x, y, -1, -1, value * value);
     }
   };
 
-  const getStyle = (i, j, htmlCol) => {
+  const searchMore = (x, y, oldX, oldY, size) => {
+    if (size === 0) { return; }
+
+    if (!(x === oldX && y === oldY) && filledSquare(x, y)) {
+      if (!(x === oldX && y === oldY) && filledSquare(x, y - 1)) {
+        searchMore(x, y - 1, x, y, size - 1);
+      }
+      if (!(x === oldX && y === oldY) && filledSquare(x, y + 1)) {
+        searchMore(x, y + 1, x, y, size - 1);
+      }
+
+      if (!(x === oldX && y === oldY) && filledSquare(x + 1, y)) {
+        searchMore(x + 1, y, x, y, size - 1);
+      }
+
+      if (!(x === oldX && y === oldY) && filledSquare(x - 1, y)) {
+        searchMore(x - 1, y, x, y, size - 1);
+      }
+    }
+  }
+
+  const getStyle = (x, y, value) => {
     let style = {};
-    if (htmlCol === 1) {
+    if (value === 1) {
       style.background = color;
       style.cursor = 'pointer';
     } else {
       style.cursor = 'not-allowed';
     }
-    if (
-      hover && (
-        i + 1 === currI && j === currJ && grid[j][i] === 1 ||
-        i - 1 === currI && j === currJ && grid[j][i] === 1 ||
-        i === currI && j + 1 === currJ && grid[j][i] === 1 ||
-        i === currI && j - 1 === currJ && grid[j][i] === 1)
-    ) {
+    if (!(x === currX && y === currY) && hover && found.includes(`${x}${y}`)) {
       style.background = highlight;
       style.animation = 'blinker 1s linear infinite';
     }
     return style;
   };
 
-  const displayValue = () => {
-    setDisplay(true);
-    setHover(true);
+  const onClick = (x, y) => {
+    if (grid[y][x] === 1) {
+      setDisplay(true);
+      setHover(false);
+    }
   };
 
   const hideValue = () => {
@@ -93,13 +114,12 @@ function App() {
     setHover(false);
   };
 
-  const getValue = (i, j) => {
-    if (i === currI && j === currJ && display) {
-      return count;
+  const getValue = (x, y) => {
+    if (x === currX && y === currY && display) {
+      return found.length;
     }
     return '';
   };
-
 
   return (
     <div className="App">
@@ -123,14 +143,15 @@ function App() {
           <Slider
             value={value}
             onChange={handleValue}
-            min={2}
-            max={7}
+            min={MIN_SIZE}
+            max={MAX_SIZE}
             step={1}
             valueLabelDisplay="auto"
+            marks={true}
           />
           <div className='grid'>
-            {grid.map((htmlRow, j) => (<div className='gridRow'>{htmlRow.map((htmlCol, i) => (
-              <div className='box' style={getStyle(i, j, htmlCol)} onClick={displayValue} onMouseOut={hideValue} onMouseOver={() => { console.log("i,j", i, j); check(i, j) }}>{getValue(i, j)}</div>
+            {grid.map((htmlRow, y) => (<div className='gridRow'>{htmlRow.map((gridItemValue, x) => (
+              <div className='box' style={getStyle(x, y, gridItemValue)} onClick={() => onClick(x, y)} onMouseOut={hideValue} onMouseOver={() => onMouseOver(x, y)}>{getValue(x, y)}</div>
             ))}</div>))}
           </div>
         </div>
